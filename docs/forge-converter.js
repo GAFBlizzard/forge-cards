4c500599f676a6a7263ec13fb93f39a392a70856
AgentElrond <gtschemer@gmail.com>
2018-08-05 06:30:53 -0400

    Update forge-converter.js

--- docs/forge-converter.js
+++ docs/forge-converter.js
@@ -12,8 +12,8 @@ function initGoogleAPI() {
    // Initialize the Google API client library.
    gapi.client.init({'apiKey': 'AIzaSyAnircUPPMsqEMmomdmkZUOnTEFyhkE9qA',
                      'discoveryDocs': ['https://sheets.googleapis.com/$discovery/rest?version=v4']}).then(function() {
-//                     'clientId': '928458196088-eqip5j7vces8s7vldntivmqi1mcn3ukq.apps.googleusercontent.com',
-//                     'scope': 'https://www.googleapis.com/auth/spreadsheets.readonly'}).then(function () {
+                     'clientId': '928458196088-eqip5j7vces8s7vldntivmqi1mcn3ukq.apps.googleusercontent.com',
+                     'scope': 'https://www.googleapis.com/auth/spreadsheets.readonly'}).then(function () {
       isGoogleAPIReady = true
    }, function(reason) {
       converterOutputArea.innerHTML = 'Failed to initialize Google API:  ' + reason
