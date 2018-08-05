ead74c42d9d2c069098bcf5ebb10342bc5023f74
AgentElrond <gtschemer@gmail.com>
2018-08-05 06:08:50 -0400

    Create forge-converter.js

---
+++ docs/forge-converter.js
@@ -0,0 +1,35 @@
+
+function setupConverter() {
+   converterOutputArea = document.getElementById("converterOutput")
+
+   isGoogleAPIReady = false
+
+   // Load the Google API client library.
+   gapi.load('client', initGoogleAPI);
+}
+
+function initGoogleAPI() {
+   // Initialize the Google API client library.
+   gapi.client.init({'apiKey': 'AIzaSyAnircUPPMsqEMmomdmkZUOnTEFyhkE9qA',
+                     'clientId': '928458196088-eqip5j7vces8s7vldntivmqi1mcn3ukq.apps.googleusercontent.com',
+                     'scope': 'https://www.googleapis.com/auth/spreadsheets.readonly'}).then(function () {
+      isGoogleAPIReady = true
+   }, function(reason) {
+      converterOutputArea.innerHTML = 'Failed to initialize Google API.'
+   })
+}
+
+function doConvert() {
+   if (isGoogleAPIReady) {
+      gapi.client.sheets.spreadsheets.values.get({
+        spreadsheetId: '1BuF4bbXN2teLlAunvpZuFGsVe9lBMcYnaVKv-NTJigo',
+        range: 'A2:K371'
+      }).then((response) => {
+         converterOutputArea.innerHTML = response.values
+      });
+   }
+   else {
+      converterOutputArea.innerHTML = 'Google API was not initialized.'
+   }
+} // end function doConvert()
+
