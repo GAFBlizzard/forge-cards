17d6dc8b487d38b09e1ed270dfa8cce2a2d63706
AgentElrond <gtschemer@gmail.com>
2018-08-06 03:23:09 -0400

    Update forge-converter.js

--- docs/forge-converter.js
+++ docs/forge-converter.js
@@ -62,7 +62,7 @@ function doConvert(holderID) {
             }
             newInnerHTML += '",\n         "name": "';
             if (response.result.values[cardIndex].length >= 2) {
-               newInnerHTML += response.result.values[cardIndex][1].replace("\"", "\\\"")
+               newInnerHTML += response.result.values[cardIndex][1].replace(/"/g, "\\\"")
             }
             newInnerHTML += '",\n         "house": "';
             if (response.result.values[cardIndex].length >= 3) {
@@ -98,7 +98,7 @@ function doConvert(holderID) {
             }
             newInnerHTML += '",\n         "text": "';
             if (response.result.values[cardIndex].length >= 11) {
-               newInnerHTML += response.result.values[cardIndex][10].replace("\"", "\\\"")
+               newInnerHTML += response.result.values[cardIndex][10].replace(/"/g, "\\\"")
             }
             newInnerHTML += '"\n      }';
          }
@@ -135,7 +135,7 @@ function doConvertToLua() {
             }
             newInnerHTML += '", name = "';
             if (response.result.values[cardIndex].length >= 2) {
-               newInnerHTML += response.result.values[cardIndex][1].replace("\"", "\\\"")
+               newInnerHTML += response.result.values[cardIndex][1].replace(/"/g, "\\\"")
             }
             newInnerHTML += '", house = "';
             if (response.result.values[cardIndex].length >= 3) {
@@ -171,7 +171,7 @@ function doConvertToLua() {
             }
             newInnerHTML += '", text = "';
             if (response.result.values[cardIndex].length >= 11) {
-               newInnerHTML += response.result.values[cardIndex][10].replace("\"", "\\\"")
+               newInnerHTML += response.result.values[cardIndex][10].replace(/"/g, "\\\"")
             }
             newInnerHTML += '" }';
          }
