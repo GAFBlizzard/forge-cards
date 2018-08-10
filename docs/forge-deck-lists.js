fc7b227ec5d919c9910bcb668408b779249a9093
AgentElrond <gtschemer@gmail.com>
2018-08-10 16:36:25 -0400

    Create forge-deck-lists.js

---
+++ docs/forge-deck-lists.js
@@ -0,0 +1,63 @@
+
+function getDeckLists() {
+   scriptOutputArea = document.getElementById("scriptOutput");
+   scriptOutputArea.innerHTML = 'Please wait...';
+
+   isGoogleAPIReady = false;
+
+   // Load the Google API client library.
+   gapi.load('client', initGoogleAPI);
+}
+
+function initGoogleAPI() {
+   // Initialize the Google API client library.
+   gapi.client.init({'apiKey': 'AIzaSyBG5VmU_QlYEGXIcCKSuEY68Cuwl-PA86Y',
+                     'discoveryDocs': ['https://sheets.googleapis.com/$discovery/rest?version=v4']}).then(function () {
+      isGoogleAPIReady = true;
+
+      getDeckListsFull();
+   }, function(reason) {
+      scriptOutputArea.innerHTML = 'Failed to initialize Google API:  ' + reason;
+   });
+}
+
+function getDeckListsFull() {
+   if (isGoogleAPIReady) {
+      gapi.client.sheets.spreadsheets.values.get({
+        spreadsheetId: '12E3C5bTjFKJaKISshQEBYRTRkSw26f6n0S1M4Qayk5w',
+        range: 'Sheet1!B:F'
+      }).then((response) => {
+         newInnerHTML = 'deckData = {\n';
+
+         for (deckIndex = (response.result.values.length - 1); deckIndex >= 0; deckIndex--) {
+            deckName = response.result.values[deckIndex][0];
+            deckFactions = response.result.values[deckIndex][1];
+            deckCardsA = response.result.values[deckIndex][2];
+            deckCardsB = response.result.values[deckIndex][3];
+            deckCardsC = response.result.values[deckIndex][4];
+            if (deckIndex != (response.result.values.length - 1)) {
+               newInnerHTML += ',\n';
+            }
+            newInnerHTML += '  { deckString = "';
+            newInnerHTML += deckName.replace(/"/g, "\\\"");
+            newInnerHTML += '~';
+            newInnerHTML += deckFactions;
+            newInnerHTML += '~';
+            newInnerHTML += deckCardsA;
+            newInnerHTML += '~';
+            newInnerHTML += deckCardsB;
+            newInnerHTML += '~';
+            newInnerHTML += deckCardsC;
+            newInnerHTML += '" }';
+         }
+
+         newInnerHTML += '\n}';
+         scriptOutputArea.innerHTML = newInnerHTML;
+      });
+   }
+   else {
+      scriptOutputArea.innerHTML = 'Google API was not initialized.';
+   }
+}
+
+
